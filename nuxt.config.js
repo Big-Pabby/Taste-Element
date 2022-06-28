7c74b054bf7e4000d19a9f58071ac7a59228e058
Victor <victoradekunle312@gmail.com>
2022-06-28 23:59:54 +0000

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: "static",
+  ssr: false,
+  generate: {
+    fallback: true,
+  },
   head: {
     title: 'Getting Started with Taste-Elements',
     htmlAttrs: {
