604c73a2d6729a32cbd171dd90af6d2a423dc1f7
Victor <victoradekunle312@gmail.com>
2022-07-02 00:34:38 +0000

    our team about done

--- pages/about.vue
+++ pages/about.vue
@@ -20,6 +20,57 @@
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis maxime atque, facilis ex, fugiat non rerum sunt ducimus perferendis sequi quod provident necessitatibus blanditiis aliquam qui praesentium? Iusto esse quis, temporibus tenetur asperiores doloribus adipisci incidunt. Consectetur nemo, eaque molestiae fugiat quo possimus esse est. Cum ea temporibus omnis, aut doloribus molestiae recusandae debitis fuga pariatur atque deserunt et excepturi, officiis architecto iste nam harum facilis fugit provident facere accusamus mollitia. Accusantium consectetur et odio ratione libero dolorum, quod perspiciatis expedita deserunt cupiditate natus quaerat. Tenetur aut, qui quisquam reiciendis impedit corporis maiores ullam pariatur natus dicta vel inventore libero.</p>
           </div>
         </div>
+
+        <div class="the-team">
+          <h1>Our Team</h1>
+          <a-carousel class="carousel" autoplay>
+            <div class="team">
+                <div class="showcase">
+                  <img src="../static/about-Image/avatar-2.jpg" alt="founder-image">
+                </div>
+                <div class="showcase-content">
+                  <h4>John Doe</h4>
+                  <p>Title 1</p>
+                </div>
+            </div>
+            <div class="team">
+                <div class="showcase">
+                    <img src="../static/about-Image/avatar-3.jpg" alt="founder-image">
+                </div>
+                <div class="showcase-content">
+                  <h4>John Doe</h4>
+                  <p>Title 2</p>
+                </div>
+            </div>
+            <div class="team">
+                <div class="showcase">
+                    <img src="../static/about-Image/chef-removebg-preview.png"  alt="founder-image">
+                </div>
+                <div class="showcase-content">
+                  <h4>John Doe</h4>
+                  <p>Title 3</p>
+                </div>
+            </div>
+            <div class="team">
+                <div class="showcase">
+                    <img src="../static/about-Image/chef3-removebg-preview.png" alt="founder-image">
+                </div>
+                <div class="showcase-content">
+                  <h4>John Doe</h4>
+                  <p>Title 4</p>
+                </div>
+            </div>
+            <div class="team">
+                <div class="showcase">
+                    <img src="../static/about-Image/chefmale.png" alt="founder-image">
+                </div>
+                <div class="showcase-content">
+                  <h4>John Doe</h4>
+                  <p>Title 5</p>
+                </div>
+            </div>
+          </a-carousel>
+        </div>
       </div>
   </div>
 </template>
@@ -48,6 +99,40 @@ export default {
   .about-showcase-content {
     text-align: right;
     max-width: 400px;
+  }
+
+  .the-team {
+    margin: 50px auto 0;
+    max-width: 500px;
+  }
+
+  .the-team h1 {
+    text-align: center;
+    padding-bottom: 5px;
+    border-bottom: 1px solid black;
+  }
+
+  .carousel {
+   max-width: 500px;
+   margin: auto;
+  }
+
+  .team {
+    height: 500px;
+    border-radius: 10px;
+    text-align: center;
+  }
+
+  h4 {
+    color: var(--color-bg-primary);
+    font-weight: bold;
+  }
+
+  .showcase {
+    width: 400px;
+    height: 400px;
+    background: #fff;
+    margin: 10px auto;
   }
 
   .about-showcase-content h1 {
