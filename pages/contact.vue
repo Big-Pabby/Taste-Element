6e0882a8177db61bbba5c6fc5530be9341715b74
Victor <victoradekunle312@gmail.com>
2022-07-06 01:51:44 +0000

    contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -24,14 +24,41 @@
                 </div>
                 <input type="submit" class="btn btn-primary" value="SUBMIT">
             </form>
+
+            <div class="contact-card">
+                <div class="card">
+                    <Icon class="card-icon" icon="ant-design:home-outlined" />
+                    <h1>Visit Us</h1>
+                    <p>Level 12, 6 Croiasure St, Melbourne, Society 1234</p>
+                </div>
+                <div class="card">
+                    <Icon class="card-icon" icon="ci:mail" />
+                    <h1>Contact Us</h1>
+                    <p><Icon icon="arcticons:google-mail" /> tasteelement@gmail.com</p>
+                    <p><Icon icon="ant-design:phone-filled" /> +234 903 006 00</p>
+                </div>
+                <div class="card">
+                    <Icon class="card-icon" icon="emojione-monotone:globe-showing-europe-africa" />
+                    <h1>Find Us Online</h1>
+                    <div class="socials">
+                        <Icon class="social-icon" icon="akar-icons:facebook-fill" />
+                        <Icon class="social-icon" icon="ant-design:twitter-circle-filled" />
+                        <Icon class="social-icon" icon="bi:instagram" />
+                        <Icon class="social-icon" icon="akar-icons:youtube-fill" />
+                    </div>
+                </div>
+            </div>
         </div>
         <Footer />
     </div>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue2';
 export default {
-
+    components: {
+        Icon
+    },
 }
 </script>
 
@@ -100,5 +127,64 @@ export default {
         padding: 15px 20px;
         border-radius: 5px;
         outline-color: var(--color-bg-secondary);
+    }
+
+    .contact-card {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 20px;
+        align-items: center;
+        margin-bottom: 50px;
+    }
+
+    .card {
+        height: 250px;
+        border-radius: 20px;
+        text-align: center;
+        padding: 20px;
+    }
+
+    .card:hover {
+        border: 2px solid var(--color-bg-primary);
+    }
+
+    .card h1 {
+        font-size: 28px;
+        margin: 20px 0;
+    }
+
+    .card-icon {
+        color: var(--color-bg-secondary);
+        font-size: 28px;
+    }
+
+    .social-icon {
+        font-size: 20px;
+        color: var(--color-bg-secondary);
+        margin-right: 15px;
+        cursor: pointer;
+        font-weight: bold;
+        transition: var(--transition);
+    }
+
+    .social-icon:hover {
+        color: var(--color-bg-primary);
+    }
+
+    @media screen and (max-width: 960px) {
+        form {
+            box-shadow: none;
+            padding: 0px;
+        }
+
+        .contact-card {
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+    @media screen and (max-width:500px) {
+
+        .contact-card {
+            grid-template-columns: 1fr;
+        }
     }
 </style>
